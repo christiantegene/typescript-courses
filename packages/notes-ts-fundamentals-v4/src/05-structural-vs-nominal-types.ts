20df8c068817bbd3a36a6891234d783920219a55
christiantegene <christiantegene@gmail.com>
2024-06-14 19:27:02 -0400

    Structural vs Nominal Typing

--- packages/notes-ts-fundamentals-v4/src/05-structural-vs-nominal-types.ts
+++ packages/notes-ts-fundamentals-v4/src/05-structural-vs-nominal-types.ts
@@ -27,10 +27,11 @@ function printCar(car: {
 }) {
   console.log(`${car.make} ${car.model} (${car.year})`)
 }
-/*
-//printCar(new Car()) //✔️ Fine
-//printCar(new Truck()) //✔️ Fine
-//printCar(vehicle) //✔️ Fine
-/**/
+
+printCar(new Car()) //✔️ Fine
+printCar(new Truck()) //✔️ Fine
+printCar(vehicle) //✔️ Fine
+
+// All 3 work because TS's type sustem is structural (as opposed to nominal), meaning as long as the argument we pass in has the same structure as the parameter in the function definition, it works.
 
 export default {}
