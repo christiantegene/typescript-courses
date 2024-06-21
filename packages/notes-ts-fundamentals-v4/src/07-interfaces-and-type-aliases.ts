14412a842e1784545435dff0896b5350ddb244a7
christiantegene <christiantegene@gmail.com>
2024-06-20 21:11:53 -0400

    Open Interfaces

--- packages/notes-ts-fundamentals-v4/src/07-interfaces-and-type-aliases.ts
+++ packages/notes-ts-fundamentals-v4/src/07-interfaces-and-type-aliases.ts
@@ -176,37 +176,35 @@ type CanBark =
 
 //* Open interfaces
 
-/*
-// function feed(animal: AnimalLike) {
-//     animal.eat
-//     animal.isAlive
-// }
-/*
-// interface AnimalLike { //✔️ Additional declaration is OK
-//     isAlive(): boolean
-// }
+
+function feed(animal: AnimalLike) {
+    animal.eat
+    animal.isAlive
+}
+
+interface AnimalLike { //✔️ Additional declaration is OK
+    isAlive(): boolean
+}
 
 //* Use case: augmenting existing types
 
-/*
-// window.document // an existing property
-// //      ^? (property) document: Document
-// window.exampleProperty = 42
-// //      ^? (property) exampleProperty: number
+window.document // an existing property
+//      ^? (property) document: Document
+window.exampleProperty = 42
+//      ^? (property) exampleProperty: number
 
-/*
-//// tells TS that `exampleProperty` exists
-// declare global {
-//     interface Window {
-//     exampleProperty: number
-//     }
-// }
+
+// tells TS that `exampleProperty` exists
+declare global {
+    interface Window {
+    exampleProperty: number
+    }
+}
 
 //* Recursive types
-/*
-// type NestedNumbers = number | NestedNumbers[]
+type NestedNumbers = number | NestedNumbers[]
 
-// const val: NestedNumbers = [3, 4, [5, 6, [7], 59], 221]
+const val: NestedNumbers = [3, 4, [5, 6, [7], 59], 221]
 /*
 // if (typeof val !== "number") {
 //   val.push(41)
